77664e627ad284912d7edc7f20326b1e14a538ff
Seegikan <62976082+Seegikan@users.noreply.github.com>
2021-10-28 23:49:14 -0500

    Examen Avance
    Sirve el agente de buena forma

--- Project/Assets/Scripts/Exam/AgentExam.cs
+++ Project/Assets/Scripts/Exam/AgentExam.cs
@@ -6,17 +6,18 @@ using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 public class AgentExam : Agent
 {
-    [SerializeField] private Transform target;
+    private Transform target;
     [SerializeField] private float agentSpeed;
     [SerializeField] private float jumpHeight;
-    [SerializeField] private float initialRewardTarget;
+    private float jump;
+    //[SerializeField] private float initialRewardTarget;
     [SerializeField] private LayerMask ground;
     [SerializeField] private Collider groundStart;
 
     [SerializeField] private ManagerGoals managerGoals;
     private MeshRenderer meshRender;
     private Rigidbody rb;
-    private float rewardTarget;
+    //private float rewardTarget;
 
     void Start()
     {
@@ -29,14 +30,12 @@ public class AgentExam : Agent
 
     public override void OnEpisodeBegin()
     {
-        rewardTarget = initialRewardTarget;
+        //rewardTarget = initialRewardTarget;
         rb.velocity = rb.angularVelocity = Vector3.zero;
         //transform.localPosition = Vector3.up * 0.5f;
         float randX = Random.Range(groundStart.bounds.min.x, groundStart.bounds.max.x);
         float randZ = Random.Range(groundStart.bounds.min.z, groundStart.bounds.max.z);
-        //ClampMagnitude()
-
-        transform.localPosition = new Vector3(randX, 0.5f, randZ);
+        transform.position = new Vector3(randX, 0.6f, randZ);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -53,41 +52,40 @@ public class AgentExam : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-
-
         //config of two actions
         Vector3 actuatorDir = Vector3.zero;
 
         actuatorDir.x = agentSpeed * actions.ContinuousActions[0];
         actuatorDir.z = agentSpeed * actions.ContinuousActions[1];
-        actuatorDir.y = actions.ContinuousActions[2];
-
-        rb.AddForce(actuatorDir);
+        // actuatorDir.y = actions.ContinuousActions[2];
+        jump = actions.ContinuousActions[2];
+        //rb.AddForce(actuatorDir);
+        actuatorDir.y = rb.velocity.y; //deja su actual
 
         //si NOOO esta en el piso 
-        if (!Physics.Raycast(transform.localPosition, Vector3.down, 0.51f, ground))
+        if (!Physics.Raycast(transform.position, Vector3.down, 0.51f, ground))
         {
-            actuatorDir.y = 0f;
+            //actuatorDir.y = 0f;
+            jump = 0f;
+
+            Debug.DrawRay(transform.position, Vector3.down * 0.51f, Color.magenta );
         }
         else
         {
-            actuatorDir.y = Mathf.Abs(Mathf.Sqrt(-2f * jumpHeight * Physics.gravity.y) * actuatorDir.y);
+            actuatorDir.y = Mathf.Abs(Mathf.Sqrt(-2f * jumpHeight * Physics.gravity.y) * jump);// actuatorDir.y);
             rb.AddForce(actuatorDir.y * Vector3.up, ForceMode.Impulse); //fuerza tomando en cuenta la masa y un soli impilso
-            actuatorDir.y = rb.velocity.y; //deja su actual
+            Debug.DrawRay(transform.position, Vector3.down * 0.51f, Color.cyan);
         }
-      
+
         //POLICY: The agents falls off the platform
         if (transform.localPosition.y < 0f)
         {
-            SetReward(-5.0f);
-           
-
-            //mazeTarget.ResetTarget();
+            SetReward(-8.0f);
+            managerGoals.ResetGoals();
             EndEpisode();
         }
-
-
-        
+        rb.velocity = actuatorDir;
+            //SetReward(-0.001f);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -107,12 +105,12 @@ public class AgentExam : Agent
             managerGoals.index++;
             target = managerGoals.goals[managerGoals.index];
             other.gameObject.SetActive(false);
-            SetReward(4f);
+            SetReward(15f);
         }
         if (other.CompareTag("Finish"))
         {
-            SetReward(20f);
-            managerGoals.ResetGoals();
+            SetReward(100f);
+            managerGoals.ResetGoals(); 
             EndEpisode();
         }
     }
